ddd844273f82b4a3c23ffe71a3d7f6d8b7eec447
Aris Valdez <samus45@hotmail.es>
2023-03-27 15:26:33 -0400

    fix connection string

--- db-config.ts
+++ db-config.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = async () => {
     try {
-        await mongoose.connect('mongodb://root:example@localhost:27017/alterna');
+        await mongoose.connect('mongodb://root:example@localhost:27017/alterna?authMechanism=DEFAULT&authSource=admin');
         console.log('Connection Success');
         
     } catch (error) {
